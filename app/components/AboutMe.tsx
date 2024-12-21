1d5c3cdf464a9010cab9edf93b3bba525013e993
Katy Senia <katy.develops@gmail.com>
2024-12-20 22:12:25 -0500

    add AboutMe component

---
+++ app/components/AboutMe.tsx
@@ -0,0 +1,10 @@
+export default function AboutME() {
+    return (
+        <section className="bg-panel-bg rounded-lg mt-4 text-foreground">
+            <h2 className="text-xl font-bol text-accent">About Me</h2>
+            <p className="text-sm text-yellowAccent mt-2">
+                Hi, I'm Katy! I'm a passionate web developer sharing my journey into the development world!
+            </p>
+        </section>
+    );
+}
