6eda6de830ac64aca2027b024f785d2d0515c6bd
Katy Senia <katy.develops@gmail.com>
2024-12-21 00:13:37 -0500

    add profile photo to about me component

--- app/components/AboutMe.tsx
+++ app/components/AboutMe.tsx
@@ -1,7 +1,17 @@
+import Image from 'next/image';
+
+
 export default function AboutMe() {
     return (
-      <section className="bg-panel-bg p-6 rounded-lg shadow-lg mt-6 text-foreground text-center">
+        <section className="bg-panel-bg p-6 rounded-lg shadow-lg mt-6 text-foreground text-center flex flex-col items-center">
         <h2 className="text-2xl font-bold text-accent mb-4">About Me</h2>
+        <Image
+        src="/Headshot.png"
+        alt="Katy's Profile Picture"
+        width={130} // Specify the width
+        height={130} // Specify the height
+        className="rounded-full mb-4 "
+      />
         <p className="text-lg text-yellowAccent leading-relaxed">
           Hi, I'm Katy! 👩‍💻 I'm a passionate web developer sharing my journey into the world of coding, design, and technology.
           <br />
