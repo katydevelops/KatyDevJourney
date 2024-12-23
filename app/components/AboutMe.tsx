c60a78db3d0e2e1c84de51fee447b56e162be8f5
Katy Senia <katy.develops@gmail.com>
2024-12-22 20:23:19 -0500

    add formatted Image to AboutMe component

--- app/components/AboutMe.tsx
+++ app/components/AboutMe.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import Headshot from '/public/HeadShot.png';
 
 
 export default function AboutMe() {
@@ -6,11 +7,12 @@ export default function AboutMe() {
         <section className="bg-panel-bg p-6 rounded-lg shadow-lg mt-6 text-foreground text-center flex flex-col items-center">
         <h2 className="text-2xl font-bold text-accent mb-4">About Me</h2>
         <Image
-        src="/Headshot.png"
+        src={Headshot}
         alt="Katy's Profile Picture"
-        width={130} // Specify the width
-        height={130} // Specify the height
-        className="rounded-full mb-4 "
+        width={130}
+        height={130}
+        className="rounded-full mb-4"
+        placeholder='blur'
       />
         <p className="text-lg text-yellowAccent leading-relaxed">
           Hi, I'm Katy! 👩‍💻 I'm a passionate web developer sharing my journey into the world of coding, design, and technology.
